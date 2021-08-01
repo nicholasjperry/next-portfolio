861a45e5fa384513345e88f823cb83054ea1ef5e
Nick Perry <nicholasjperry1@gmail.com>
2021-08-01 17:16:44 -0500

    Clarifying

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "744aa81b95b11f36de83";
+/******/ 	var hotCurrentHash = "7e266cc9cb8524b9c30f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
